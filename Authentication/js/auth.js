6e2d5a1ad2f1f2da51b5a43515a06e9cbbaf0227
Mahditalal173211 <122090595+Mahditalal173211@users.noreply.github.com>
2024-04-27 12:37:34 +0300

    Update auth.js

--- Authentication/js/auth.js
+++ Authentication/js/auth.js
@@ -155,7 +155,7 @@ function createNewUser(
             } else if (userType === "donor") {
               localStorage.setItem("email", email);
               localStorage.setItem("userType", "donor");
-              window.location.href = "../Donor/donorHomePage.html";
+              window.location.href = "../Donor/donorhomePage.html";
             }
           })
           .catch((error) => {
